8cdbdabaea8c487033d4ce8447f8b7c5ae7a4fc6
Ben-Employsure <113003446+Ben-Employsure@users.noreply.github.com>
2023-08-14 15:53:39 +1000

    Update event-scripts.js

--- event-scripts.js
+++ event-scripts.js
@@ -111,3 +111,14 @@ Genesys("subscribe", "MessagingService.messagesReceived", function ({ data }) {
   }
 });
 }
+
+window.addEventListener("load", function () {
+  let phoneBtns = document.querySelectorAll("[href*='tel:']");
+  // Find all phone links
+  phoneBtns.forEach(function (phoneBtn){
+    // When link is clicked push phone click event
+    phoneBtn.addEventListener('click', function () {
+      dataLayer.push({ event: "phoneClick" });
+    });
+  });
+});
